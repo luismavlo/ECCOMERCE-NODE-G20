8fdd59e76b00c34824c6e4368920c76525fd8f3b
Luis Miguel <lmavendano1597@gmail.com>
2023-01-24 13:50:02 -0500

    se agregaron los enviroments en el archivo db.js

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize({
-  dialect: 'postgres',
-  host: 'localhost',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: 'eccomerceDb',
+  database: process.env.DB_DATABASE,
   logging: false,
 });
 
