ef84a9e52fa9642309768c759e3eb9ea03dbe542
Luis Miguel <lmavendano1597@gmail.com>
2023-01-25 10:53:07 -0500

    funcionalidad crud usuarios realizada

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,30 +1,107 @@
-const findUsers = (req, res) => {
+const User = require('../models/user.model');
+
+const findUsers = async (req, res) => {
+  const users = await User.findAll({
+    where: {
+      status: true,
+    },
+  });
+
   res.json({
     status: 'success',
+    message: 'Users was found successfully',
+    users,
   });
 };
 
-const findUser = (req, res) => {
-  res.json({
+const findUser = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findOne({
+    where: {
+      status: true,
+      id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'User not found',
+    });
+  }
+
+  res.status(200).json({
     status: 'success',
+    message: 'User was found successfully',
+    user,
   });
 };
 
-const createUser = (req, res) => {
-  res.json({
+const createUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email: email.toLowerCase(),
+    password,
+  });
+
+  res.status(201).json({
     status: 'success',
+    message: 'User created successfully',
+    user,
   });
 };
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { username, email } = req.body;
+
+  const user = await User.findOne({
+    where: {
+      status: true,
+      id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'User not found',
+    });
+  }
+
+  await user.update({ username, email });
+
   res.json({
     status: 'success',
+    message: 'User updated successfully',
   });
 };
 
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findOne({
+    where: {
+      status: true,
+      id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'User not found',
+    });
+  }
+
+  await user.update({ status: false });
+
   res.json({
     status: 'success',
+    message: 'User deleted successfully',
   });
 };
 
