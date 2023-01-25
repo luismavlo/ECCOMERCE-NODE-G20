f5b8a3e2a17bd9348bb0435927679be1b8f4319e
Luis Miguel <lmavendano1597@gmail.com>
2023-01-24 20:35:34 -0500

    funcionalidad de actualizar y obtener productos realizada

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,16 +1,40 @@
 const Product = require('../models/product.model');
 
-exports.findProducts = (req, res) => {
-  res.json({
+exports.findProducts = async (req, res) => {
+  const products = await Product.findAll({
+    where: {
+      status: true,
+    },
+  });
+
+  res.status(200).json({
     status: 'success',
-    message: 'ROUTE - GET DESDE EL CONTROLADOR',
+    message: 'The products found were successfully',
+    products,
   });
 };
 
-exports.findProduct = (req, res) => {
-  res.json({
+exports.findProduct = async (req, res) => {
+  const { id } = req.params;
+
+  const product = await Product.findOne({
+    where: {
+      id,
+      status: true,
+    },
+  });
+
+  if (!product) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'The product was not found',
+    });
+  }
+
+  return res.status(200).json({
     status: 'success',
-    message: 'ROUTE FIND PRODUCT BY ID',
+    message: 'The product was found successfully',
+    product,
   });
 };
 
@@ -18,8 +42,8 @@ exports.createProduct = async (req, res) => {
   const { title, description, quantity, price, categoryId, userId } = req.body;
 
   const newProduct = await Product.create({
-    title,
-    description,
+    title: title.toLowerCase(),
+    description: description.toLowerCase(),
     quantity,
     price,
     categoryId,
@@ -33,10 +57,37 @@ exports.createProduct = async (req, res) => {
   });
 };
 
-exports.updateProduct = (req, res) => {
-  res.json({
+exports.updateProduct = async (req, res) => {
+  //1. OBTENGO MI ID DE LA REQ.PARAMS
+  const { id } = req.params;
+  //2. OBTENER LA INFORMACION A ACTUALIZAR DE LA REQ.BODY
+  const { title, description, quantity, price } = req.body;
+  //3. BUSCAR EL PRODUCTO A ACTUALIZAR
+  const product = await Product.findOne({
+    where: {
+      id,
+      status: true,
+    },
+  });
+  //4. SI NO EXISTE EL PRODUCTO ENVIAMOS UN ERROR
+  if (!product) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'The product was not found',
+    });
+  }
+  //5. SI TODO SALIO BIEN, ACTUALIZAMOS EL PRODUCTO ENCONTRADO
+  const updatedProduct = await product.update({
+    title,
+    description,
+    quantity,
+    price,
+  });
+  //6. ENVIO LA RESPUESTA AL CLIENTE
+  res.status(200).json({
     status: 'success',
-    message: 'ROUTE - PATCH',
+    message: 'Then product has been updated successfully',
+    updatedProduct,
   });
 };
 
