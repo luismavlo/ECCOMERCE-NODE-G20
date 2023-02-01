b1a27c91d1e15e1133de99a28d00ad2825bc9879
Luis Miguel <lmavendano1597@gmail.com>
2023-01-31 19:01:56 -0500

    error handling finalizado

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,11 +1,56 @@
+const AppError = require('../utils/appError');
+
+const handleCastError22P02 = err => {
+  console.log('entra');
+  message = 'Some type of data sent does not match what was expected';
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  //dejar esto arriba
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    //Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log Error
+    console.error('ERROR 🧨', err);
+    // 2) SEND GENERIC MESSAGE
+    res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'fail';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    console.log(err.parent.code);
+    let error = { ...err };
+    if (error.parent.code === '22P02') error = handleCastError22P02(error);
+
+    sendErrorProd(error, res);
+  }
 };
 
 module.exports = globalErrorHandler;
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -25,39 +25,23 @@ const findUser = catchAsync(async (req, res) => {
   });
 });
 
-const createUser = async (req, res) => {
-  try {
-    //1. OBTENER LA INFORMACION DE LA REQ.BODY
-    const { username, email, password, role = 'user' } = req.body;
-    //2. CREAR EL USUARIO CON LA INFORMACION DE LA REQ.BODY
-    const user = await User.create({
-      username: username.toLowerCase(),
-      email: email.toLowerCase(),
-      password,
-      role,
-    });
-    //3. ENVIAR UNA RESPUESTA AL USUARIO
-    res.status(201).json({
-      status: 'success',
-      message: 'User created successfully',
-      user,
-    });
-  } catch (error) {
-    console.log(error.parent.code);
-
-    if (error.parent.code === '22P02') {
-      return res.status(400).json({
-        status: 'error',
-        message: 'Invalid DataType in your request',
-      });
-    }
-
-    return res.status(500).json({
-      status: 'fail',
-      message: 'Internal server error',
-    });
-  }
-};
+const createUser = catchAsync(async (req, res) => {
+  //1. OBTENER LA INFORMACION DE LA REQ.BODY
+  const { username, email, password, role = 'user' } = req.body;
+  //2. CREAR EL USUARIO CON LA INFORMACION DE LA REQ.BODY
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email: email.toLowerCase(),
+    password,
+    role,
+  });
+  //3. ENVIAR UNA RESPUESTA AL USUARIO
+  res.status(201).json({
+    status: 'success',
+    message: 'User created successfully',
+    user,
+  });
+});
 
 const updateUser = catchAsync(async (req, res) => {
   const { username, email } = req.body;
